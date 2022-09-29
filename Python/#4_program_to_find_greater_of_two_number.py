6234e1f6cc941d9641b1889352f9bc5352e53874
write2program <113636205+write2program@users.noreply.github.com>
2022-09-29 23:13:33 +0530

    #4

--- Python/#4_program_to_find_greater_of_two_number.py
+++ Python/#4_program_to_find_greater_of_two_number.py
@@ -4,6 +4,7 @@ if num1 < num2:
     print('{1} is greater than {0}'.format(num1,num2))
 else:
     print('{0} is greater than {1}'.format(num1,num2))
+    
 #Output: 30 is greater than 20
     
     
