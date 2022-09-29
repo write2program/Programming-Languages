29fbbda5b589cb8502a4ace59ee9fc8d4aab83fa
write2program <113636205+write2program@users.noreply.github.com>
2022-09-29 23:08:53 +0530

    #1

--- Python/#1_Python_program_to_add_two_numbers.py
+++ Python/#1_Python_program_to_add_two_numbers.py
@@ -1,6 +1,6 @@
 num1 = 10
 num2 = 20
 sum = num1 + num2 #Adding two numbers
-print('Sum of {0} and {1} is {2}'.format(num1,num2,sum))
+print('Sum of {0} and {1} is {2}.'.format(num1,num2,sum))
 
-#Output: Sum of 10 and 20 is 30
+#Output: Sum of 10 and 20 is 30.
